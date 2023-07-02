7bd217f249f7063d496baba659d671966b6410ca
wiasliaw <wiasliaw@protonmail.com>
2023-07-02 15:35:42 +0800

    feat(deployment): deployment script and info

---
+++ script/GoerliDeployment.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "forge-std/Script.sol";
+import "../src/TransactionMetadataEntry.sol";
+import "../src/mocks/MockHandler.sol";
+
+contract Deployment is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        vm.startBroadcast(deployerPrivateKey);
+        MockHandler handler = new MockHandler();
+        TransactionMetadataEntry entry = new TransactionMetadataEntry();
+        vm.stopBroadcast();
+    }
+}
